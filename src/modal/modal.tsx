b08ad0116003df8fdac77a3b6fb83fc9bdb5da8a
HiiroHK <gabrielsresende18@gmail.com>
2025-06-05 20:31:06 -0300

    reparo na tabela do modal

--- src/modal/modal.tsx
+++ src/modal/modal.tsx
@@ -13,10 +13,30 @@ export function Modal() {
           <p>Dashboard</p>
           <p>receitas</p>
           <p>Despesas</p>
+          <button>Sair</button>
         </aside>
-        <h1>Receitas</h1>
-        <button>Filtrar vendas</button>
-        <button>Filtrar</button>
+        <div className="flex justify-between">
+          <h1>Receitas</h1>
+          <button className="border rounded-xl">Filtrar vendas</button>
+          <button className="bg-blue-600 border border-blue-500 rounded-xl">
+            Filtrar
+          </button>
+          <button className="border rounded-xl">Nova venda</button>
+        </div>
+      </div>
+      <div className="flex">
+        <div>
+          <p className="border">Cliente</p>
+          <p>Jander</p>
+        </div>
+        <div>
+          <p className="border">Curso</p>
+          <p>Fullstack</p>
+        </div>
+        <div>
+          <p className="border">Valor</p>
+          <p>2000,00</p>
+        </div>
       </div>
     </div>
   );
